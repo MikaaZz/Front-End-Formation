346411feeb6fd37ddb209e25d260a1d0ab3c9ec7
Mikhael Zimmermann <mikhael928@gmail.com>
2022-03-09 17:45:42 -0300

    Update index.js

--- JavaScript_programando_a_Orientação_a_Objetos/index.js
+++ JavaScript_programando_a_Orientação_a_Objetos/index.js
@@ -1,22 +1,41 @@
-class Cliente{
+class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
 } // Classe é o molde
 
+class ContaCorrente { //Representa
+    agencia;
+    saldo;
+
+    sacar(valor) { // parenteses mostra que é operação e o "valor" serve para fazer as contas
+        if (this.saldo >= valor) { // this = desta ( no caso, conta corrente )
+            this.saldo -= valor; // pega o valor da conta corrente e subtrai o valor sacado
+        } else {
+            console.log("Voce nao possui este valor em conta!")
+        }
+    }
+}
+
+// Cliente é o objeto
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
-cliente1.nome = "Ricardo"; // Cliente é o objeto
+cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1);
-console.log(cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0; // .saldo = chamar o saldo para lhe dar um valor
+contaCorrenteRicardo.agencia = 1001;
+
+console.log("Seu saldo é de: ", contaCorrenteRicardo.saldo, "R$"); // imprime o valor da conta corrente
+
+contaCorrenteRicardo.saldo += 100; // adiciona 100 na conta corrente
+contaCorrenteRicardo.saldo += 372;
+console.log("Seu saldo é de: ", contaCorrenteRicardo.saldo, "R$"); // imprime novo valor da conta corrente
+contaCorrenteRicardo.sacar(92); // seta o valor da operação dentro do ()
+console.log("Seu saldo é de: ", contaCorrenteRicardo.saldo, "R$"); // imprime a conta com o valor sacado ( negativa )
+
+//console.log(cliente1);
+//console.log(cliente2);
